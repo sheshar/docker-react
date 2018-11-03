b64eb04bc340eee3510187e97fe7fcbff59749ce
Sheshagiri Padmanabha Rao <sheshagiripadmanabharao@Sheshagiris-MacBook-Pro.local>
2018-11-03 14:27:51 +0530

    updated app.js in feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello Dear My lovely anuradha 
+            Hello Anuradha How Are You?
           </p>
           <a
             className="App-link"
